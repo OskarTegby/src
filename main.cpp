6a208d6206702e4af64b8b2bc245f5b28e638a2a
majaarvola <majaarvola@gmail.com>
2020-05-06 15:56:08 +0200

    Revert "Added scrolling"
    This reverts commit f66d350d8e8b3a24dd07fbe52d67f096db000e4d.

--- main.cpp
+++ main.cpp
@@ -210,12 +210,6 @@ void moveTrackball(Context *ctx, int x, int y)
     }
 }
 
-void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
-    zoomFactor += yoffset / 100.0f;
-    if (zoomFactor < 0) zoomFactor = 0;
-    if (zoomFactor * fovy > M_PI && projectionToggle == 1) zoomFactor = M_PI / fovy;
-}
-
 void errorCallback(int /*error*/, const char* description)
 {
     std::cerr << description << std::endl;
@@ -303,7 +297,6 @@ int main(void)
     glfwSetMouseButtonCallback(ctx.window, mouseButtonCallback);
     glfwSetCursorPosCallback(ctx.window, cursorPosCallback);
     glfwSetFramebufferSizeCallback(ctx.window, resizeCallback);
-    glfwSetScrollCallback(ctx.window, scrollCallback);
 
     // Load OpenGL functions
     glewExperimental = true;
