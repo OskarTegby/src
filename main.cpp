f66d350d8e8b3a24dd07fbe52d67f096db000e4d
OskarTegby <oskar.tegby@icloud.com>
2020-05-06 15:53:22 +0200

    Added scrolling
    You know what scrolling is, right?

--- main.cpp
+++ main.cpp
@@ -210,6 +210,12 @@ void moveTrackball(Context *ctx, int x, int y)
     }
 }
 
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+    zoomFactor += yoffset / 100.0f;
+    if (zoomFactor < 0) zoomFactor = 0;
+    if (zoomFactor * fovy > M_PI && projectionToggle == 1) zoomFactor = M_PI / fovy;
+}
+
 void errorCallback(int /*error*/, const char* description)
 {
     std::cerr << description << std::endl;
@@ -297,6 +303,7 @@ int main(void)
     glfwSetMouseButtonCallback(ctx.window, mouseButtonCallback);
     glfwSetCursorPosCallback(ctx.window, cursorPosCallback);
     glfwSetFramebufferSizeCallback(ctx.window, resizeCallback);
+    glfwSetScrollCallback(ctx.window, scrollCallback);
 
     // Load OpenGL functions
     glewExperimental = true;
